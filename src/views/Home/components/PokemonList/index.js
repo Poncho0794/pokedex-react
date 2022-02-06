3e40f45f4c824e9c3b08a5d8f7489b3b56ef2c82
Alfonso Banda <benbanda07@gmail.com>
2022-02-05 23:22:08 -0600

    Minor Changes

--- src/views/Home/components/PokemonList/index.js
+++ src/views/Home/components/PokemonList/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import PokemonListItem from "./PokemonListItem";
 import { getPokemonIdFromUrl } from "../../../../utils";
 import "./style.css";
